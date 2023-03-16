997564cd6d131262ca535c862ecf4d7eff81beae
Mitchell Finlay <mitchell.finlay00@gmail.com>
2023-03-16 22:19:49 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
+
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+    if (input) {
+      input.addEventListener('click', clickAlert);
+    }
+  }
+  
+
+  addingEventListener();
